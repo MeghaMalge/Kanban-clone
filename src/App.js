5c4851d65f8f09bae747c2bcce020f35572a1c42
Megha Malge <meghamalge226@gmail.com>
2023-06-18 03:13:38 +0530

    added env variable

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SideDrawer from "./components/SideDrawer";
 import "./layout.css";
 
 function App() {
+  console.log(process.env.REACT_APP_BASE_URL);
   return (
     <div className="container">
       <section className="left-container">
